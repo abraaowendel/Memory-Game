3c39ec2e4524454f4f4ef983c3817f472941e0f2
Abraao Wendel <abraao.dev@gmail.com>
2022-07-01 15:43:23 -0300

    organizando arquivos

---
+++ src/components/InfoItem/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    margin: 20px 0;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+export const Label = styled.div`
+    font-size: 15px;
+    color: #6A7D88;
+`;
+
+export const Value = styled.div`
+    font-size: 37px;
+    font-weight: bold;
+`;
+
+
